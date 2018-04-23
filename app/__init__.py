6dbfb38fa5d5354aadd2486450d6f95391b36e3e
Jordan Cooper <jordan.cooper3000@gmail.com>
2018-04-23 01:13:44 -0500

    add db configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,26 @@
 from flask import Flask
+from flask_sqlalchemy  import SQLAlchemy
+import os , psycopg2
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'v\xf9\xf7\x11\x13\x18\xfaMYp\xed_\xe8\xc9w\x06\x8e\xf0f\xd2\xba\xfd\x8c\xda'
+app.config.from_object(__name__)# Flask-Login login manager
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+login_manager.session_protection = "strong"
+
+UPLOAD_FOLDER = './app/static/uploads'
+DATABASE_URL = os.environ['DATABASE_URL']
+
+app.config['SECRET_KEY'] = 'pH0t0Gr@l^l'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] 	= True
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+db = SQLAlchemy(app)
 
 from app import views
