c758f1c552918dd61a507ca29c42acb59d6e22c4
Jordan Cooper <jordan.cooper3000@gmail.com>
2018-04-28 22:32:46 -0500

    update secret keys

--- app/__init__.py
+++ app/__init__.py
@@ -15,11 +15,11 @@ login_manager.session_protection = "strong"
 UPLOAD_FOLDER = './app/static/uploads'
 DATABASE_URL = os.environ['DATABASE_URL'] #'postgresql://user[:password]@localhost/photogram'
 
-app.config['SECRET_KEY'] = 'pH0t 0Gr@l^l'
+app.config['SECRET_KEY'] = 's\x97\xc2y\x9f\xb2l6\x17\x0f\xf4\x00R\xe6\x16\xff\xccSl\xda\x93\xde]%'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] 	= True
-app.config['TOKEN_SECRET'] = 't0k3ns3cr3+'
+app.config['TOKEN_SECRET'] = '\xff\xd4\xd5\xef<\xa4F\xcc\xff\xd7H\xa7?n\x1d\xbbtP\xdfj\xff\x8d\x90\xd7'
 
 
 conn = psycopg2.connect(DATABASE_URL)
