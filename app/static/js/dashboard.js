ed8bff14e0620901bef5065ae46ac48bfcd389c8
Jordan Cooper <jordan.cooper3000@gmail.com>
2018-05-07 09:41:15 -0500

    add default post photo

--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -150,7 +150,7 @@ Vue.component('card', {
    
                 <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
                 <div class="card card-inverse card-info">
-                    <a href=""><img class="card-img-top" :src="photo"></a>
+                    <a href=""><img class="card-img-top" src="http://2018.cedesc.com.br/assets/uploads/thumb-default.png"></a>
                     <div class="card-block">
                         <figure class="profile profile-inline">
                             <img src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif" class="profile-avatar" alt="">
